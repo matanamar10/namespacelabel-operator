74c6a702afec8aad2a4e1472ad1fcdcc31b8a18b
matanamar10 <matanamar76@gmail.com>
2024-11-07 16:38:33 +0200

    Fix: add all the internal packages to the

--- internal/controller/namespacelabel_controller.go
+++ internal/controller/namespacelabel_controller.go
@@ -43,6 +43,7 @@ type NamespacelabelReconciler struct {
 // +kubebuilder:rbac:groups=labels.dana.io,resources=namespacelabels,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=labels.dana.io,resources=namespacelabels/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=labels.dana.io,resources=namespacelabels/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
